5d892f6b8e2f0f8528f9aef723d6e80fcb694e09
Pejman Hadavi <pejmanhadavi77@gmail.com>
2020-11-17 22:42:54 +0330

    Add class validator configurations.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Swagger Configrations
+  // Swagger configurations
   const options = new DocumentBuilder()
     .setTitle('Base store')
     .setDescription('Base store APIs')
@@ -14,6 +15,13 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
+
+  // Class Validator configurations
+  app.useGlobalPipes(
+    new ValidationPipe({
+      disableErrorMessages: true,
+    }),
+  );
 
   await app.listen(3000);
 }
