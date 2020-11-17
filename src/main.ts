cdf897329550310ebf85feaf685eadcc18ac6c59
Pejman Hadavi <pejmanhadavi77@gmail.com>
2020-11-17 22:10:59 +0330

    Add swagger configrations.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger Configrations
+  const options = new DocumentBuilder()
+    .setTitle('Base store')
+    .setDescription('Base store APIs')
+    .setVersion('1.0')
+    .addTag('base-store')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
