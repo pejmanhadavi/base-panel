2b6dc23c574d35b2edf624d17ed6b8f6471fe193
Pejman Hadavi <pejmanhadavi77@gmail.com>
2020-12-21 15:13:04 +0330

    Add coupon schema.

---
+++ src/modules/coupons/schemas/coupon.schema.ts
@@ -0,0 +1,34 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import * as mongoose from 'mongoose';
+import { Product } from 'src/modules/products/schemas/product.schema';
+import { Category } from 'src/modules/categories/schemas/category.schema';
+
+export type CouponDocument = Coupon & Document;
+
+@Schema({ versionKey: false, timestamps: true })
+export class Coupon {
+  @Prop({ type: String, maxlength: 256 })
+  name: string;
+
+  @Prop({ type: Number, required: true, default: 0 })
+  amount: number;
+
+  @Prop({ type: Date })
+  expiresAt?: Date;
+
+  @Prop({ type: Number })
+  usedCount?: number;
+
+  @Prop({
+    type: [{ type: mongoose.Types.ObjectId, ref: Product.name }],
+  })
+  products: Product[];
+
+  @Prop({
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: Category.name, index: true }],
+  })
+  categories: Category[];
+}
+
+export const CouponSchema = SchemaFactory.createForClass(Coupon);
